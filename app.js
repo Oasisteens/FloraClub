02c24b52a20a66a581f7cba5079244cc1ed34ac5
LQ458 <yihao.qin17311-biph@basischina.com>
2023-09-29 10:58:16 +0800

    System building

---
+++ app.js
@@ -0,0 +1,183 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const p = 'mongodb+srv://Leo:QYH7809ABC@cluster0.bkxswvb.mongodb.net/';
+
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('./models/user');
+const Post = require('./models/post');
+
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+// Increase the maximum number of event listeners for the emitter object
+emitter.setMaxListeners(20);
+
+// Add event listeners to the emitter object
+emitter.on('event1', () => {
+  // Event handler code
+});
+
+emitter.on('event2', () => {
+  // Event handler code
+});
+
+// ...
+// Other event listener attachments
+// ...
+
+// Make sure to remove event listeners when they are no longer needed
+emitter.off('event2', listener); // Example of removing an event listener
+
+
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username }); // add await
+            if (!user || user.password !== password) {
+                return done(null, false, {message: 'Incorrect username or password'})
+            }
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id); // Changed to User
+        done(null, user);
+    } catch(error) {
+        done(error);
+    }
+})
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'floraclub',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.set('view engine', 'ejs');
+app.listen(8080, '0.0.0.0', () => {
+    console.log('Server is running on port 8080');
+});
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login')
+}
+
+function checkLoginAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/dashboard');
+}
+
+mongoose.connect(p, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+app.use(express.static('public'));
+
+
+app.get('/register', (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.redirect('/dashboard');
+    }
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    console.log(req.body);  // Log the received data
+
+    try {
+        if (!req.body.username || !req.body.password) {
+            return res.status(400).send('Username or password missing');
+        }
+
+        const user = new User(req.body);
+        await user.save();
+        res.redirect('/login');
+    } catch (error) {
+        console.error('Error during registration:', error.message);
+        res.status(500).send('Internal server error');
+    }
+});
+
+
+
+
+app.get('/login', (req, res) => {
+    const ip_address = req.ip || req.connection.remoteAddress;
+    console.log(ip_address);
+    if (req.isAuthenticated()) {
+        return res.redirect('/dashboard');
+    }
+    res.render('login');
+});
+
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: 'dashboard',
+    failureRedirect: '/login'
+}));
+
+app.get('/dashboard', checkAuthenticated, (req, res) => {
+    res.render('dashboard', {username: req.user.username})
+})
+
+app.get('/',async (req, res) => {
+    res.render('intro');
+})
+
+app.get('/general', checkAuthenticated, async (req, res) => {
+    const generalPosts = await Post.find({ group: 'general' });
+    const reversedPosts = generalPosts.reverse();
+    res.render('general', { reversedPosts, username: req.user.username });
+})
+
+
+app.post('/general', (req, res) => {
+    const post = new Post(req.body);
+    post.save().then((result) => {
+        res.redirect('/general')
+    })
+});
+
+app.post('/generalDeletePost', async (req, res) => {
+    console.log('Post delete invoked' )
+    console.log(req.body.postId)
+    const deleteId = req.body.postId;
+    try {
+        await Post.deleteOne({ _id: deleteId });
+        res.redirect('/general');
+    } catch (error) {
+        console.error('Error during post deletion:', error.message);
+        res.status(500).send('Internal server error');
+    }
+});
+
+
+app.use((req, res, next) => {
+    res.status(404).render('404');
+})
