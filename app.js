bf03d9f03d714e627721302699461c613df4af73
Aiercroft <127703641+Aiercroft@users.noreply.github.com>
2023-10-17 09:14:07 +0800

    d

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
-const mongooseLink = 'mongodb+srv://jess:jess@cluster0.cgg9ypb.mongodb.net/?retryWrites=true&w=majority'
-
+const mongooseLink = 'mongodb+srv://yihaoqin:qyh7809abc@cluster0.6enxioa.mongodb.net/'
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 const bodyParser = require('body-parser')
+
+mongoose.connect(mongooseLink, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+    });
