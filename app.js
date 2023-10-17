9b3cb9e10deeecf58b95eeb289c48bc710a618b5
Aiercroft <127703641+Aiercroft@users.noreply.github.com>
2023-10-17 09:38:59 +0800

    initialization

--- app.js
+++ app.js
@@ -16,3 +16,53 @@ mongoose.connect(mongooseLink, { useNewUrlParser: true, useUnifiedTopology: true
     .catch(error => {
         console.error('Error connecting to MongoDB:', error);
     });
+
+    passport.use(new LocalStrategy(
+        async (username, password, done) => {
+            try{
+                const user = await User.findOne({ username });
+                if (!user || user.password !== password) {
+                    return done(null, false, {message: 'Incorrect username or password'})
+                }
+                return done(null, user);
+            } catch(error) {
+                return done(error);
+            }
+        }
+    ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id); // Changed to User
+            done(null, user);
+        } catch(error) {
+            done(error);
+        }
+    })
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(session({
+        secret: 'floraCode',
+        resave: false,
+        saveUninitialized: false
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.set('view engine', 'ejs');
+    app.listen(3000, '0.0.0.0', ()  => {
+        console.log("Server running on port 3000");
+    });
+
+    function checkAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect('/login')
+    };
+
+    app.use(express.static('public'));
