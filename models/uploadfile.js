86a7a7a4d7f185f3039c9a66ca3c503008d55c18
LQ458 <yihao.qin17311-biph@basischina.com>
2023-10-20 14:42:47 +0800

    img preview

--- models/uploadfile.js
+++ models/uploadfile.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Set the destination folder for uploaded files
+    cb(null, './public/uploads/'); // Set the destination folder for uploaded files
   },
   filename: function (req, file, cb) {
     cb(null, 'biphflora-' + req.body.featuredColumnTitle + '-' + req.user.username + '.png' );
