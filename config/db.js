19b2b18493aef949195abfa0cbf24becab331bdd
LQ <yihao.qin17311-biph@basischina.com>
2024-02-28 22:27:56 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: "mongodb://root:biphflora123@112.74.58.221:27017/?retryWrites=true&w=majority",
+    url: "mongodb://yourserver:27017/?retryWrites=true&w=majority",
     database: "filesys",
     imgBucket: "photos",
   };
